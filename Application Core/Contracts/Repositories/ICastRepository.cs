d8a3681af3c0a54db5df046e7355380e178f7157
HermioneHe03 <103541745+HermioneHe03@users.noreply.github.com>
2022-05-26 02:39:37 -0400

    Add files via upload

--- Application Core/Contracts/Repositories/ICastRepository.cs
+++ Application Core/Contracts/Repositories/ICastRepository.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Application_Core.Entities;
 
 namespace Application_Core.Contracts.Repositories
 {
-    public interface ICastRepository
+    public interface ICastRepository: IRepository<Cast>
     {
+        Task<List<int>> GetMovieIdsById(int id);
+        Task<List<Movie>> GetMoviesById(int id);
+        Task<List<Cast>> GetCastsByMovie(int movieId);
+        Task<List<MovieCast>> GetMovieCastsByMovie(int movieId);
     }
 }
