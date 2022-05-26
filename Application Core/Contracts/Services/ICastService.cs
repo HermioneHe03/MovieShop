791cdb55d0290fe72eaa6f0bb4d63576440fa6f9
HermioneHe03 <103541745+HermioneHe03@users.noreply.github.com>
2022-05-26 02:40:02 -0400

    Add files via upload

--- Application Core/Contracts/Services/ICastService.cs
+++ Application Core/Contracts/Services/ICastService.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Application_Core.Models;
 
 namespace Application_Core.Contracts.Services
 {
     public interface ICastService
     {
-
+        Task<CastDetailsResponseModel> GetCastDetails(int id);
     }
 }
--- Application Core/Contracts/Services/IGenreService.cs
+++ Application Core/Contracts/Services/IGenreService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Application_Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace Application_Core.Contracts.Services
 {
     public interface IGenreService
     {
+        Task<List<Genre>> GetAllGenres();
     }
 }
