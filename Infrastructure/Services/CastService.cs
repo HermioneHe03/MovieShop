29e26c6544e9f6dbb6634495e645d6a89ce51319
HermioneHe03 <103541745+HermioneHe03@users.noreply.github.com>
2022-05-26 02:41:37 -0400

    Add files via upload

--- Infrastructure/Services/CastService.cs
+++ Infrastructure/Services/CastService.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Application_Core.Contracts.Repositories;
+using Application_Core.Entities;
+using Application_Core.Models;
 
 namespace Infrastructure.Services
 {
     public class CastService
     {
+        private readonly ICastRepository _castRepository;
+
+        public CastService(ICastRepository castRepository)
+        {
+            _castRepository = castRepository;
+        }
+
+        public async Task<CastDetailsResponseModel> GetCastDetails(int id)
+        {
+            Cast cast = await _castRepository.GetById(id);
+            List<Movie> moviesOfCast = await _castRepository.GetMoviesById(id);
+            CastDetailsResponseModel castDetails = CastDetailsResponseModel.FromEntity(cast, moviesOfCast);
+
+            return castDetails;
+        }
     }
 }
--- Infrastructure/Services/GenreService.cs
+++ Infrastructure/Services/GenreService.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Application_Core.Contracts.Services;
+using Application_Core.Entities;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
 
 namespace Infrastructure.Services
 {
-    public class GenreService
+    public class GenreService : Repository<Genre>, IGenreService
     {
+        public GenreService(MovieShopDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<List<Genre>> GetAllGenres()
+        {
+            var genres = await GetAll();
+            var genresList = new List<Genre>();
+
+            foreach (var genre in genres)
+            {
+                genresList.Add(new Genre
+                {
+                    Id = genre.Id,
+                    Name = genre.Name
+                });
+            }
+
+            return genresList;
+        }
     }
 }
